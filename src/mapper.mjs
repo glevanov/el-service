bd68ee2d9be397c00f9a0c7557a774bb20c97ec5
Grigoriy Levanov <gregory@levanov.io>
2023-11-25 10:56:59 +0100

    trim output by current hour

--- src/mapper.mjs
+++ src/mapper.mjs
@@ -1,4 +1,25 @@
-import { compareAsc, getDate } from 'date-fns'
+import { compareAsc, getDate, getHours } from 'date-fns'
+
+export const trimByCurrentHour = (data) => {
+  const today = new Date()
+  const hour = getHours(today)
+  const day = getDate(today)
+  
+  return data.reduce((trimmed, current) => {
+    if (current.day === day) {
+      const index = current.points.findIndex((point) => point.hour === hour)
+      const twoHoursBeforeIndex = index - 2
+      return [
+        ...trimmed,
+        {
+          day: current.day,
+          points: current.points.slice(twoHoursBeforeIndex > 0 ? twoHoursBeforeIndex : 0)
+        }
+      ]
+    }
+    return [...trimmed, current]
+  }, [])
+}
 
 const mapPoint = (point) => ({
   hour: point.position - 1,
@@ -6,12 +27,13 @@ const mapPoint = (point) => ({
 })
 
 export const mapData = (data) => {
-  const periods = data.Publication_MarketDocument.TimeSeries
+  const timeSeries = data.Publication_MarketDocument.TimeSeries
+  const periods = (Array.isArray(timeSeries) ? timeSeries : [timeSeries])
     .map(ts => ts.Period)
     .sort((a, b) => compareAsc(new Date(a.timeInterval.start), new Date(b.timeInterval.start)))
   const points = periods.map(period => ({
     day: getDate(new Date(period.timeInterval.start)),
     points: period.Point.map(mapPoint),
   }))
-  return points
+  return trimByCurrentHour(points)
 }
